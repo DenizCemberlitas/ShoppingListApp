b47a1043e5fb6cd09f5d2b8697018e3312ec26db
Deniz Cemberlitas <150486385+DenizCemberlitas@users.noreply.github.com>
2026-03-06 19:08:34 +0100

    Remove firebase-config.js from tracking

--- public/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBCs_sGQmsyqcqH0evj4_ugikTSJBQN0Jc",
-  authDomain: "shoppinglist-denz.firebaseapp.com",
-  projectId: "shoppinglist-denz",
-  storageBucket: "shoppinglist-denz.firebasestorage.app",
-  messagingSenderId: "665278314342",
-  appId: "1:665278314342:web:2ed44ab9d736b467fa7aa7"
-};
